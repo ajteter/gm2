30fb03a1a3c28184e03fc9772873f0d64420a84f
Steammmmm <ajteter@hotmail.com>
2025-08-26 11:22:26 +0800

    referer

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,11 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="zh-CN">
 			<head>
+				<meta name="referrer" content="origin" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+				<meta name="format-detection" content="telephone=no" />
+				<meta name="apple-mobile-web-app-capable" content="yes" />
+				<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 				<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6779881482191995" crossOrigin="anonymous"></script>
 			</head>
 			<body>
